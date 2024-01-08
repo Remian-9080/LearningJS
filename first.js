dcaa15ee1931ce971ab5828f0b79bb8355495d71
Remian 9080 <rakibpsp@gmail.com>
2024-01-08 19:39:23 +0600

    Updated

--- first.js
+++ first.js
@@ -1,1 +1,15 @@
-console.log("Mohammed");
+
+const student={
+     name:"Rakib",
+     Id: 192
+}
+let r="Rakib";
+for(let i of r)
+{
+    console.log("val of I is", i);
+}
+console.log(typeof(student));
+for( let j in student)
+{
+    console.log(student[j]);
+}
